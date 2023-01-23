e83cccadda036199065570c9442bcd4bbfd87173
alanisleby <mxs1920@gmail.com>
2023-01-22 20:57:39 -0500

    resolviendo conflictos en la clase Integrantes

--- src/entidades/Integrante.java
+++ src/entidades/Integrante.java
@@ -8,10 +8,8 @@ package entidades;
  *
  * @author USER1
  */
-<<<<<<< HEAD
+
 public abstract class Integrante extends Persona{
-=======
-public class Integrante extends Persona{
     Comunidad comunidad;
     Area[] areas;
     
@@ -19,7 +17,6 @@ public class Integrante extends Persona{
     void mostrar() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
->>>>>>> cc88134d007d67ac30a9beb64e956e7c171899f0
     
     //no se como hacer un enum de actividad.
     abstract void llenarHorasActividad();
