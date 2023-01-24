be2df083d74dd30f5679924e699eb69c0151b710
mjca <mjcald@gmail.com>
2023-01-23 22:11:39 -0600

    Codigo inicial para guardar Integrante Se agrega Area para el Lider

--- src/entidades/Lider.java
+++ src/entidades/Lider.java
@@ -8,19 +8,37 @@ package entidades;
  *
  * @author USER1
  */
-public class Lider extends Persona{
-
-    public void evaluarPostulante(){
-        
+public class Lider extends Persona {
+    public Lider(Area area) {
+        this.area = area;
     }
-    void retirarIntegrante(){
-        
+
+    Area area;
+
+    public Area getArea() {
+        return area;
+    }
+
+    public void setArea(Area area) {
+        this.area = area;
+    }
+
+    public void evaluarPostulante() {
+
+    }
+
+    void retirarIntegrante() {
+
+    }
+
+    void guardarIntegrante(Integrante integrante) {
+        area.integrantes.add(integrante);
     }
 
     @Override
     void mostrar() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Not supported yet."); // To change body of generated methods, choose
+                                                                       // Tools | Templates.
     }
 
 }
-
