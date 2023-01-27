923065ffb83adb171fc49f52d649aa8114c10e31
Adrian <adrian.capchaq@gmail.com>
2023-01-26 23:48:08 -0500

    se agregó JFramePrincipal

--- src/main/Test.java
+++ src/main/Test.java
@@ -26,16 +26,22 @@ public class Test {
         
         //ArrayList<Integrante> integrantes = new ArrayList<>();
         //integrantes.add(new Integrante("Pepito", "20", "724633333"));
+        Comunidad conovatec=new Comunidad("Conovatec");
         ArrayList<Area> areas = new ArrayList<>();
+        
         Area desarrolloSoftware=new Area("Desarrollo de Software");
         areas.add(desarrolloSoftware);
-        Comunidad conovatec=new Comunidad("Conovatec");
         Lider lider1=new Lider(desarrolloSoftware, "adrian", "24", "777777");
         lider1.guardarIntegrante(new Integrante("Juan", "20", "2312312"));
-        lider1.getArea().mostrarIntegrantes();
-        System.out.println("");
+        //lider1.getArea().mostrarIntegrantes();
+        //desarrolloSoftware.mostrarIntegrantes();
         
-        desarrolloSoftware.mostrarIntegrantes();
+        Area analisisDatos=new Area("Análisis de Datos");
+        areas.add(analisisDatos);
+        Lider lider2=new Lider(analisisDatos, "Segundo", "24", "88888");
+        lider2.guardarIntegrante(new Integrante("Pepe", "23", "66666"));
+        lider2.getArea().mostrarIntegrantes();
+        
         
     }
     
