7381b99510b5ac231718e9fc2ea258fa934806e4
ETERRAZAS21PE <eterrazas.met.pe21@gmail.com>
2023-01-30 06:05:49 +0000

    actualizando vista

---
+++ src/vistas/Ventana01.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vistas;
+
+/**
+ *
+ * @author eterr
+ */
+public class Ventana01 {
+    
+    public static void main (String[] args) {
+        vistas.frmVentana01 x = new vistas.frmVentana01();
+        x.setVisible(true);
+    }
+    
+}
